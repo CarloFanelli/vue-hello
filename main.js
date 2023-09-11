535732fc784320104cfde1a0735ff77f942f3e24
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-11 14:31:20 +0200

    print in page message

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { createApp } = Vue
 
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. */
+
   createApp({
     data() {
       return {
 
-        
+        message : 'ciao user',
 
     }
     }
